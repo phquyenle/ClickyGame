e1e607eabd4841432e0ea592e9f2641437ce2be3
Quyen Le <heyquyenle@gmail.com>
2020-06-29 19:03:56 -0700

    Updates

---
+++ precache-manifest.c5e57a246a2adb32e003a81cc4915823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb5a3ec2e31ba42931ee77fd9b53b46",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "9bd4549045b383873a48",
+    "url": "/ClickyGame/static/css/main.e4cda853.chunk.css"
+  },
+  {
+    "revision": "2bd0f7eaf8daae6a35fc",
+    "url": "/ClickyGame/static/js/2.c581d9da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyGame/static/js/2.c581d9da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bd4549045b383873a48",
+    "url": "/ClickyGame/static/js/main.e87861f2.chunk.js"
+  },
+  {
+    "revision": "52ba01ea425dc5e7e4bc",
+    "url": "/ClickyGame/static/js/runtime-main.33951b4a.js"
+  }
+]);
